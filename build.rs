4cc3f15d72dac923ae99b795f6e07dbf9fd40397
Robin Mirow <robinmirow@live.com>
2017-07-29 14:11:26 +0200

    Remove check for existing libs in target directory

--- build.rs
+++ build.rs
@@ -4,26 +4,4 @@ fn main()
 {
     // give linker correct path
     println!("cargo:rustc-link-search=.\\lib");
-
-    let profile = std::env::var("PROFILE").unwrap();
-
-    #[allow(unused_assignments)]
-    let mut target_dir = "";
-
-    if profile == "release"
-    {
-        target_dir = ".\\target\\release";
-    }
-    else
-    {
-        target_dir = ".\\target\\debug";
-    }
-
-    if !Path::new(&(target_dir.to_owned() + "\\plugins")).exists()
-        || !Path::new(&(target_dir.to_owned() + "\\libvlc.dll")).exists()
-        || !Path::new(&(target_dir.to_owned() + "\\libvlccore.dll")).exists()
-    {
-        print!("Warning! Libvlc does not exist in ouput directory.");
-        panic!("Aborting");
-    }
 }
